771df3f1760af288a1ae024a4ab11533741535d7
Duc Tung <conin121298@gmail.com>
2019-03-30 00:41:46 +0700

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,7 @@
 import 'bootstrap';
 import 'slick-carousel';
-import './app/slide.js';
-import './app/offCanvas.js';
-import './app/scrollTop';
 import '../scss/include.scss';
-import '../scss/lib/ihover.css';
+
 
 
 
